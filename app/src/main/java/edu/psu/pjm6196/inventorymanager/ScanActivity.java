68205fcb441c7e20c2c46fe8e9bd0a70545abed3
Patrick Miller <paddymills@proton.me>
2023-02-27 15:50:15 -0500

    initial designs of main and scan activities

---
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/ScanActivity.java
@@ -0,0 +1,14 @@
+package edu.psu.pjm6196.inventorymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
---
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/SettingsFragment.java
@@ -0,0 +1,13 @@
+package edu.psu.pjm6196.inventorymanager;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
