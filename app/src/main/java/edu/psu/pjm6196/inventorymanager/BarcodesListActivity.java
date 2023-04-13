5461f641600311e735e72d45f11f2c171d58678b
Patrick Miller <paddymills@proton.me>
2023-04-12 20:34:14 -0400

    impl display fragment

--- app/src/main/java/edu/psu/pjm6196/inventorymanager/BarcodesListActivity.java
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/BarcodesListActivity.java
@@ -2,10 +2,13 @@ package edu.psu.pjm6196.inventorymanager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +20,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import edu.psu.pjm6196.inventorymanager.db.Barcode;
+import edu.psu.pjm6196.inventorymanager.db.BarcodeDatabase;
 import edu.psu.pjm6196.inventorymanager.db.BarcodeViewModel;
 
 public class BarcodesListActivity extends AppCompatActivity {
@@ -38,6 +42,17 @@ public class BarcodesListActivity extends AppCompatActivity {
         barcodeViewModel.getAllBarcodes().observe(this, adapter::setBarcodes);
     }
 
+    public void displayMaterial(int id) {
+        BarcodeDatabase.getBarcode(id, barcode -> {
+            Bundle args = new Bundle();
+            args.putInt("barcode_id", barcode.id);
+            // TODO: add args
+
+            BarcodeDisplayFragment barcodeDisplay = new BarcodeDisplayFragment();
+            barcodeDisplay.setArguments(args);
+            barcodeDisplay.show(getSupportFragmentManager(), "barcodeDisplay");
+        });
+    }
 
     public class BarcodeListAdapter extends RecyclerView.Adapter<BarcodeListAdapter.BarcodeViewHolder> {
 
@@ -50,9 +65,7 @@ public class BarcodesListActivity extends AppCompatActivity {
                 titleView = view.findViewById(R.id.barcodeTitle);
 
                 // set listeners
-                titleView.setOnClickListener(v -> {
-                    Log.d("BarcodeViewHolder", "BarcodeViewHolder: clicked");
-                });
+                view.setOnClickListener(v -> displayMaterial(barcode.id));
             }
         }
 
@@ -96,5 +109,28 @@ public class BarcodesListActivity extends AppCompatActivity {
             notifyDataSetChanged();
         }
 
+    }
+
+    public static class BarcodeDisplayFragment extends DialogFragment {
+        int barcode_id;
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            barcode_id = getArguments().getInt("barcode_id");
+            final String title = getArguments().getString("material");
+            final String heat = getArguments().getString("heat");
+
+            builder
+                .setTitle(title)
+                    .setMessage(heat)
+                    .setPositiveButton("Edit", (dialog, id) -> ((BarcodesListActivity) getActivity()).edit(barcode_id))
+                    .setNeutralButton("Delete", (dialog, id) -> {})
+                    .setNegativeButton("Cancel", (dialog, id) -> {});
+
+            return builder.create();
+        }
     }
 }
