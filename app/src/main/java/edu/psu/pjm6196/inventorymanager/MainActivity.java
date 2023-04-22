9f0a6c47f8d632257038dfd2308c4978e6855343
Patrick Miller <paddymills@proton.me>
2023-04-22 14:27:04 -0400

    add settings icon

--- app/src/main/java/edu/psu/pjm6196/inventorymanager/MainActivity.java
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             .setOnClickListener(
                 v -> startActivity(new Intent(this, BarcodesListActivity.class))
             );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            startActivity(new Intent(this, SettingsFragment.class));
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
