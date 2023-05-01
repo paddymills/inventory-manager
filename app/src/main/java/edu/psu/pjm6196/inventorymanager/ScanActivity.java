28c529fc99d23e213894bffe12b74af6e8cc5297
Patrick Miller <paddymills@proton.me>
2023-05-01 08:42:29 -0400

    remove old analyzer

--- app/src/main/java/edu/psu/pjm6196/inventorymanager/ScanActivity.java
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/ScanActivity.java
@@ -1,13 +1,10 @@
 package edu.psu.pjm6196.inventorymanager;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.OptIn;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ExperimentalGetImage;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
-import androidx.camera.core.resolutionselector.ResolutionSelector;
-import androidx.camera.core.resolutionselector.ResolutionStrategy;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
@@ -16,19 +13,12 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.Size;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.demo.CameraXViewModel;
 
 import android.Manifest;
-import android.widget.Button;
+import android.util.Log;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-
-import edu.psu.pjm6196.inventorymanager.barcodescanner.BarcodeAnalyzer;
 import edu.psu.pjm6196.inventorymanager.barcodescanner.BarcodeScannerProcessor;
 import edu.psu.pjm6196.inventorymanager.barcodescanner.utils.GraphicOverlay;
 
@@ -39,8 +29,6 @@ public class ScanActivity extends CustomAppCompatActivity {
 
     private static final String[] CAMERA_PERMISSION = new String[] {Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
-
-    private boolean scanning_is_bound;
 
     public static final CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
     public static final int cameraLens = CameraSelector.LENS_FACING_BACK;
@@ -53,12 +41,12 @@ public class ScanActivity extends CustomAppCompatActivity {
     private BarcodeScannerProcessor barcodeProcessor;
     private boolean needUpdateGraphicOverlayImageSourceInfo;
 
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        Log.i(TAG, "onCreate");
 
         previewView = findViewById(R.id.scan_preview);
         graphicOverlay = findViewById(R.id.scan_overlay);
@@ -139,62 +127,6 @@ public class ScanActivity extends CustomAppCompatActivity {
             });
 
         cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis);
-    }
-
-    private void init_camera_old() {
-        findViewById(R.id.btn_scan).setOnClickListener(v -> toggle_scanner_binding());
-
-//        previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
-        previewView.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
-
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-        Executor executor = ContextCompat.getMainExecutor(this);
-        cameraProviderFuture.addListener(this::toggle_scanner_binding, executor);
-
-    }
-
-    private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
-        imageAnalysis =
-            new ImageAnalysis.Builder()
-//                .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER)
-//                .setImageQueueDepth(3)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .setResolutionSelector(
-                    new ResolutionSelector.Builder()
-                        .setResolutionStrategy(
-                            new ResolutionStrategy(new Size(previewView.getWidth(), previewView.getHeight()), ResolutionStrategy.FALLBACK_RULE_CLOSEST_LOWER)
-                        )
-                        .build()
-                )
-                .build();
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new BarcodeAnalyzer(this));
-
-        preview = new Preview.Builder().build();
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview);
-    }
-
-    private void toggle_scanner_binding() {
-        try {
-            ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-            if ( scanning_is_bound ) {
-                Log.d(TAG, "Scanning barcode...");
-
-                cameraProvider.unbindAll();
-                ((Button) findViewById(R.id.btn_scan)).setText(R.string.scan_capture_unbound);
-            } else {
-                bindImageAnalysis(cameraProvider);
-                ((Button) findViewById(R.id.btn_scan)).setText(R.string.scan_capture_bound);
-            }
-
-            scanning_is_bound = !scanning_is_bound;
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     private boolean hasCameraPermission() {
