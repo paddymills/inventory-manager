b13530685b346bb92a1eaa3f3f14c2050f0a723e
Patrick Miller <paddymills@proton.me>
2023-06-14 10:27:18 -0400

    package processor

--- app/src/main/java/edu/psu/pjm6196/inventorymanager/barcodescanner/processor/VisionImageProcessor.java
+++ app/src/main/java/edu/psu/pjm6196/inventorymanager/barcodescanner/processor/VisionImageProcessor.java
@@ -14,10 +14,12 @@
  * limitations under the License.
  */
 
-package edu.psu.pjm6196.inventorymanager.barcodescanner;
+package edu.psu.pjm6196.inventorymanager.barcodescanner.processor;
 
 import android.graphics.Bitmap;
+
 import androidx.camera.core.ImageProxy;
+
 import com.google.mlkit.common.MlKitException;
 
 import java.nio.ByteBuffer;
@@ -25,18 +27,28 @@ import java.nio.ByteBuffer;
 import edu.psu.pjm6196.inventorymanager.barcodescanner.graphics.GraphicOverlay;
 import edu.psu.pjm6196.inventorymanager.barcodescanner.utils.FrameMetadata;
 
-/** An interface to process the images with different vision detectors and custom image models. */
+/**
+ * An interface to process the images with different vision detectors and custom image models.
+ */
 public interface VisionImageProcessor {
 
-  /** Processes a bitmap image. */
-  void processBitmap(Bitmap bitmap, GraphicOverlay graphicOverlay);
+    /**
+     * Processes a bitmap image.
+     */
+    void processBitmap(Bitmap bitmap, GraphicOverlay graphicOverlay);
 
-  /** Processes ByteBuffer image data, e.g. used for Camera1 live preview case. */
-  void processByteBuffer(ByteBuffer data, FrameMetadata frameMetadata, GraphicOverlay graphicOverlay) throws MlKitException;
+    /**
+     * Processes ByteBuffer image data, e.g. used for Camera1 live preview case.
+     */
+    void processByteBuffer(ByteBuffer data, FrameMetadata frameMetadata, GraphicOverlay graphicOverlay) throws MlKitException;
 
-  /** Processes ImageProxy image data, e.g. used for CameraX live preview case. */
-  void processImageProxy(ImageProxy image, GraphicOverlay graphicOverlay) throws MlKitException;
+    /**
+     * Processes ImageProxy image data, e.g. used for CameraX live preview case.
+     */
+    void processImageProxy(ImageProxy image, GraphicOverlay graphicOverlay) throws MlKitException;
 
-  /** Stops the underlying machine learning model and release resources. */
-  void stop();
+    /**
+     * Stops the underlying machine learning model and release resources.
+     */
+    void stop();
 }
